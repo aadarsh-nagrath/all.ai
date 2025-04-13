06e3d0a0c7c4ab64bc85c3474f12dfdb6736fa0f
aadarsh-nagrath <anagrath1@gmail.com>
2025-04-13 23:56:16 +0530

    build issue fix

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans, GeistMono } from "geist/font";
 import { ThemeProvider } from "@/components/theme-provider";
 import SessionWrapper from "@/components/session-wrapper";
 import "./globals.css";
@@ -9,15 +9,8 @@ import { Toaster } from "@/components/ui/toaster"
 import { connectDB } from "@/db/connect";
 connectDB();
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const geistSans = GeistSans;
+const geistMono = GeistMono;
 
 export const metadata: Metadata = {
   title: "All.Ai",
